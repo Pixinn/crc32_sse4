a23cb7b392134df722463b7ea07c7d716bd83d18
Christophe Meneboeuf <christophe@xtof.info>
2017-05-07 22:40:26 +0200

    C++ test

--- include/crc32c.h
+++ include/crc32c.h
@@ -9,4 +9,13 @@
 #include <cstdlib>
 
 
+
+/// \brief Returns true if SSE4.2 instruction set is available
+bool isSSE42Available();
+/// \brief Initializes the CRC - 32C lookup table for software - based CRC calculation
+void initCrcTable();
+/// \brief Hardware based (SSE4.2) CRC calculation
 uint32_t hwCrc32c(uint32_t initialCrc, const char *buf, size_t len);
+/// \brief Software based CRC calculation
+uint32_t swCrc32c(uint32_t initialCrc, const char *buf, size_t len);
+
---
+++ test/test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+#include "crc32c.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    if (!isSSE42Available()) {
+        cout << "SSE4.2 unsupported: cannot proced to hardware calculation.\nFAILURE!\n";
+        return -1;
+    }
+
+    const string str("SSE4-CRC32: A hardware accelerated CRC32 implementation for node.js");
+    
+    const auto crcHw = hwCrc32c(0, str.data(), str.size());
+    
+    initCrcTable();
+    const auto crcSw = swCrc32c(0, str.data(), str.size());
+
+    cout << "RESULTS:\nHardware = " << to_string(crcHw) << "\nSoftware = " << to_string(crcSw);
+    if (crcSw == crcHw) {
+        cout << "\nSUCCESS!\n";
+        return 0;
+    }
+    else {
+        cout << "\nFAILURE!\n";
+        return -1;
+    }
+}
